aaf256e9711d0914f1c3001c58902a60706025a0
Adnan Kurt <adnankurt16@gmail.com>
2022-07-19 00:01:49 +0300

    online db connection

--- db/db_connection.js
+++ db/db_connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/restful_api",{useUnifiedTopology:true,useNewUrlParser:true})
+mongoose.connect('mongodb+srv://adnankurt:vmtmr2oa0vdOLfaa@cluster0.ur5gnxp.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>console.log("veritabanına bağlanıldı"))
 .catch(err=>console.log("sorun oluştu"+err));
