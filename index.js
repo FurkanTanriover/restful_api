a44d80590775546c83689e1b77302de4a914df70
furkantanriover <furkan.tanrioveer@gmail.com>
2022-07-19 15:01:53 +0300

    delete and update operations created

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ require("./db/db_connection");
 
 //routes
 
-const userRouter=require("./router/user_router")
+const userRouter = require("./router/user_router");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use("/api/users",userRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/users", userRouter);
 
 app.listen("30022", () => {
   console.log("server 30022 portundan ayaklandırıldı");
 });
 
-app.get("/",(req,res)=>{
-    res.status(200).json({"mesaj":"hoşgeldiniz"});
+app.get("/", (req, res) => {
+  res.status(200).json({ mesaj: "hoşgeldiniz" });
 });
-
--- router/user_router.js
+++ router/user_router.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const user = require("../models/user_model");
 const User = require("../models/user_model");
 
 router.get("/", async (req, res) => {
@@ -15,19 +16,36 @@ router.post("/", async (req, res) => {
   try {
     const addUser = new User(req.body);
     const resp = await addUser.save();
-
     return res.status(200).json({ data: "Eklendi" });
   } catch (err) {
     return res.status(404).json({ error: err, errorMessage: err.message });
   }
 });
 
-router.patch("/:id", (req, res) => {
-  res.json(req.body);
+router.patch("/:id", async (req, res) => {
+  try {
+    const resp = await User.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true, runValidators: true });
+    if (resp) {
+      return res.status(200).json({ data: "User updated successfully" });
+    } else {
+      return res.status(404).json({ data: "User not found" });
+    }
+  } catch (err) {
+    return res.status(404).json({ error: err, errorMessage: err.message });
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  res.json({ mesaj: `id'si ${req.params.id} olan kullanıcı silinecek` });
+router.delete("/:id", async (req, res) => {
+  try {
+    const resp = await User.findByIdAndDelete({ _id: req.params.id });
+    if (resp) {
+      return res.status(200).json({ data: "User deleted succesfully" });
+    } else {
+      return res.status(404).json({ data: "User not found" });
+    }
+  } catch (err) {
+    return res.status(404).json({ error: err, errorMessage: err.message });
+  }
 });
 
 module.exports = router;
